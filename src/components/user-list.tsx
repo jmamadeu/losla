0ddb712db9575341cd2c7445ff8d860eeab3ac2c
João Amadeu <jmamadeu2000@gmail.com>
2021-05-28 11:22:00 +0100

    :zap: feat: inital commit

---
+++ src/components/user-list.tsx
@@ -0,0 +1,27 @@
+import { FC } from 'react';
+
+import { useQuery, gql } from '@apollo/client';
+
+const EXCHANGE_RATES = gql`
+  query GetExchangeRates {
+    rates(currency: "USD") {
+      currency
+      rate
+    }
+  }
+`;
+
+export const UserList: FC = () => {
+  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.rates.map(({ currency, rate }) => (
+    <div key={currency}>
+      <p>
+        {currency}: {rate}
+      </p>
+    </div>
+  ));
+};
